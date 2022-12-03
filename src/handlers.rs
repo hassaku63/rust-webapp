82e006ff9b825b07ecd9dc6a87ead24d3800a719
hassaku63 <hassaku63@gmail.com>
2022-12-03 13:21:45 +0900

    wip: red test

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,13 +1,14 @@
 use axum::{
-    extract::Extension,
+    extract::{Extension, Path, path},
     http::StatusCode,
     response::IntoResponse,
-    Json,
+    Json
 };
 use std::sync::Arc;
 use crate::repositories::{
     CreateTodo,
     TodoRepository,
+    UpdateTodo,
 };
 
 pub async fn create_todo<T: TodoRepository>(
@@ -17,4 +18,31 @@ pub async fn create_todo<T: TodoRepository>(
     let todo = repo.create(payload);
 
     (StatusCode::CREATED, Json(todo))
+}
+
+pub async fn find_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    Extension(repo): Extension<Arc<T>>,
+) -> Result<impl IntoResponse, StatusCode> {
+    todo!();
+    Ok(StatusCode::OK)
+}
+
+pub async fn all_todo<T: TodoRepository>(
+    Extension(repo): Extension<Arc<T>>,
+) -> impl IntoResponse {
+    todo!()
+}
+
+pub async fn update_todo<T: TodoRepository>(
+    Extension(repo): Extension<Arc<T>>,
+) -> impl IntoResponse {
+    todo!()
+}
+
+pub async fn delete_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    Extension(repo): Extension<Arc<T>>,
+) -> impl IntoResponse {
+    todo!()
 }
--- src/repositories.rs
+++ src/repositories.rs
@@ -36,6 +36,13 @@ pub struct CreateTodo {
     text: String,
 }
 
+#[cfg(test)]
+impl CreateTodo {
+    pub fn new(text: String) -> Self {
+        Self { text: text }
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
 pub struct UpdateTodo {
     text: Option<String>,
